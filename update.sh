7599397dc9afcad4c09cc876172b0bb4fda52f2a
N-rwal <104368706+N-rwal@users.noreply.github.com>
2026-03-12 10:03:09 +0100

    Refactor update script to use variables for paths

--- update.sh
+++ update.sh
@@ -3,16 +3,19 @@
 clear
 echo "=== Start update ==="
 
+WS="$HOME/ros2_ws"
+MAP_DIR="$WS/src/go2_ros2_light/go2_robot_sdk/map"
+
 if ls office* 1> /dev/null 2>&1; then
     echo "Moving map files..."
-    rm -f /home/pcip/ros2_ws/src/go2_ros2_light/go2_robot_sdk/map/*
-    mv office* /home/pcip/ros2_ws/src/go2_ros2_light/go2_robot_sdk/map/
+    rm -f "$MAP_DIR"/*
+    mv office* "$MAP_DIR/"
 else
     echo "Only old maps will be used"
 fi
 
 echo "Step 2: Building workspace..."
-cd /home/pcip/ros2_ws
+cd "$WS"
 colcon build
 
 echo "Step 3: Sourcing setup..."
